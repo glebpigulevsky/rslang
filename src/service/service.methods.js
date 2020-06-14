9f815a9df904ad946c50f56700465fe3efb6643e
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-06-14 21:52:10 +0300

    feat: add methods.js

---
+++ src/service/service.methods.js
@@ -0,0 +1,13 @@
+import Settings from './endpoints/service.endpoints.settings';
+import Statistics from './endpoints/service.endpoints.statistics';
+import Users from './endpoints/service.endpoints.users';
+import UserWords from './endpoints/service.endpoints.userWords';
+import Words from './endpoints/service.endpoints.words';
+
+export {
+    Settings,
+    Statistics,
+    Users,
+    UserWords,
+    Words
+}
