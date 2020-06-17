5fa4b9a93150147426335b16a4a5902abd2db6e9
CasperCarver <pigulevsky.gleb@ya.ru>
2020-06-17 12:03:00 +0300

    feat: add fetch to words collection

--- src/components/main/components/mainGame/mainGame.js
+++ src/components/main/components/mainGame/mainGame.js
@@ -1,11 +1,17 @@
 import './scss/some_component.scss';
 import { GAME_BLOCK, TEMPLATE_MAIN_GAME } from '../../common/main.constants';
+import {
+  SettingsApi, StatisticsApi, UsersApi, UserWordsApi, WordsApi,
+} from '../../../../services/services.methods';
 
+const wordsApi = new WordsApi();
 const game = {
 
-  addMdGameScreen() {
+  async addMdGameScreen() {
     GAME_BLOCK.innerHTML = '';
     GAME_BLOCK.append(TEMPLATE_MAIN_GAME.content.cloneNode(true));
+    const res = await wordsApi.getWordsCollection({ group: 0, page: 1 });
+    console.log(res);
   },
 
   init() {
