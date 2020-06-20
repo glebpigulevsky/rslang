849b8d1c0da4f96fdfae6b20e808a01ff9f3babc
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-06-20 17:56:54 +0300

    fix: test withot rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'no-underscore-dangle': 'off',
     'import/prefer-default-export': 'off',
     'class-methods-use-this': 'off',
     'no-param-reassign': [2, { props: false }],
