3a5a3454e30770a0b4950da7f63e68e2ee204c60
maxshelepen <maxshelepen25@gmail.com>
2020-06-21 11:48:48 +0300

    init: game development start

---
+++ src/components/sprint/main.app.js
@@ -0,0 +1,26 @@
+import Menu from './main_menu';
+import { CLASS_NAMES } from '../../common/common.constants';
+import MAIN_GREETINGS from './common/main.constants';
+
+import './scss/main.styles.scss';
+
+const burgerMenu = new Menu();
+burgerMenu.init();
+
+class Main {
+  constructor() {
+    this.logoContent = null;
+    this.logoElement = null;
+  }
+
+  sayHello() {
+    this.logoElement.textContent = this.logoContent;
+  }
+
+  init() {
+    this.logoElement = document.querySelector(`.${CLASS_NAMES.MAIN.LOGO}`);
+    this.logoContent = MAIN_GREETINGS;
+  }
+}
+
+export default new Main();
