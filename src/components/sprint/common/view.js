31409f3ddc66470517026915f668b53b7da64d20
nixon2105 <radivilov1991@mail.ru>
2020-06-23 23:28:49 +0300

    feat: add game inner

--- src/components/sprint/common/view.js
+++ src/components/sprint/common/view.js
@@ -6,9 +6,14 @@ class View {
   renderDOM() {
     document.querySelector('body').innerHTML = '';
     document.querySelector('body').insertAdjacentHTML('afterbegin',
-      `<div class="body__game-sprint">
+      `<div class="body__game-sprint body__game-sprint--background">
         <div class="wrapper wrapper__sprint-game">
-          <div class="inner__game-sprint"></div>
+          <div class="inner__game-sprint">
+            <div class="game-sprint__header">
+                <div class="game-sprint__score">Score: <span class="score__value"></span></div>
+                <div class="game-sprint__timer"></div>
+            </div>
+          </div>
         </div>
       </div>`);
   }
