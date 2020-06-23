dfb821fc6a6cadffbb1e1207af8124f5d8dea390
nixon2105 <radivilov1991@mail.ru>
2020-06-23 23:57:09 +0300

    feat: add timer of the game sprint

--- src/components/sprint/common/view.js
+++ src/components/sprint/common/view.js
@@ -5,17 +5,19 @@ class View {
 
   renderDOM() {
     document.querySelector('body').innerHTML = '';
-    document.querySelector('body').insertAdjacentHTML('afterbegin',
+    document.querySelector('body').insertAdjacentHTML(
+      'afterbegin',
       `<div class="body__game-sprint body__game-sprint--background">
         <div class="wrapper wrapper__sprint-game">
           <div class="inner__game-sprint">
             <div class="game-sprint__header">
-                <div class="game-sprint__score">Score: <span class="score__value"></span></div>
+                <div class="game-sprint__score">Score: <span class="score__value">120</span></div>
                 <div class="game-sprint__timer"></div>
             </div>
           </div>
         </div>
-      </div>`);
+      </div>`,
+    );
   }
 }
 
--- src/components/sprint/sprint.app.js
+++ src/components/sprint/sprint.app.js
@@ -1,8 +1,11 @@
 import View from './common/view';
+import Timer from './common/timer';
 import './scss/sprint.styles.scss';
 
+const timerJS = new Timer();
 const sprintGameInit = () => {
   View.init();
+  timerJS.init();
 };
 
 export default sprintGameInit;
