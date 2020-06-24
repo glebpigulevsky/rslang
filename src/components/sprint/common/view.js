8bf441ec8fd321f70334689eb59f91560fbd043a
nixon2105 <radivilov1991@mail.ru>
2020-06-24 13:26:02 +0300

    fix: fix slylelint

--- src/components/sprint/common/view.js
+++ src/components/sprint/common/view.js
@@ -14,6 +14,25 @@ class View {
                 <div class="game-sprint__score">Score: <span class="score__value">120</span></div>
                 <div class="game-sprint__timer"></div>
             </div>
+            <div class="game-sprint__rating">
+              <img src="https://img.icons8.com/nolan/50/goal.png"/>
+              <img src="https://img.icons8.com/nolan/50/goal.png"/>
+              <img src="https://img.icons8.com/nolan/50/goal.png"/>
+              <img src="https://img.icons8.com/nolan/50/goal.png"/>
+            </div>
+            <div class="game-sprint__field">
+              <div class="game-sprint__word game-sprint__word--eng">eng word</div>
+              <div class="game-sprint__word game-sprint__word--rus">rus word</div>
+              <div class="game-sprint__controls">
+                <button class="sprint__btn sprint__btn--wrong">wrong</button>
+                <button class="sprint__btn sprint__btn--right">right</button>
+              </div>
+            </div>
+            <div class="game-sprint__footer">
+              <p class="game-sprint__instruction">You can use the keyboard to control it</p>
+              <img src="https://img.icons8.com/ultraviolet/40/000000/previous.png"/>
+              <img src="https://img.icons8.com/ultraviolet/40/000000/next.png"/>
+            </div>
           </div>
         </div>
       </div>`,
