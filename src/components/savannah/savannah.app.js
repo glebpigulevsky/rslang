6f7eb798e54b1f5244bcb9710eea168df7b39960
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-07-05 21:35:46 +0300

    feat: add savannah start

--- src/components/savannah/savannah.app.js
+++ src/components/savannah/savannah.app.js
@@ -2,6 +2,7 @@ import './scss/savannah.styles.scss';
 
 const savannahApp = () => {
   document.querySelector('.main-header').style.display = 'none';
+  // document.querySelector('.savannah__question').classList.add('savannah__question_move');
 };
 
-export default savannahApp();
+export default savannahApp;
