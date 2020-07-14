158ca80fe9572a281f077d979f93ec877be4112c
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-07-14 13:51:35 +0300

    refactor: rename classes team page

--- src/components/main/pages/team-page.component.js
+++ src/components/main/pages/team-page.component.js
@@ -2,89 +2,82 @@ import '../../team/scss/team.styles.scss';
 
 export const teamPageComponent = {
   render: () => `
-  <div class="row">
+  <div class="xteam__row">
 
-  <div class="column">
-    <div class="card">
+  <div class="xteam__column">
+    <div class="xteam__card">
       <img src="https://avatars2.githubusercontent.com/u/30927000?s=460&u=efd4485c38e4409a1ef99a8663b43ec6ff66b84f&v=4" alt="Jane" style="width:100%">
-      <div class="container">
+      <div class="xteam__container">
         <h2>Aleh Serhiyenia</h2>
-        <p class="title">Mentor</p>
+        <p class="title">Mentor <a class="xteam__button" href="https://github.com/pulya10c">Github</a></p> 
         <p>Mentoring the team.</p>
-        <p><button class="team__button">Github</button></p>
       </div>
     </div>
   </div>
 
-  <div class="column">
-    <div class="card">
+  <div class="xteam__column">
+    <div class="xteam__card">
       <img src="https://avatars2.githubusercontent.com/u/50077360?s=460&u=dfa2cb20ebd2870a64b32d76974c1a1551f94946&v=4" alt="Mike" style="width:100%">
-      <div class="container">
+      <div class="xteam__container">
         <h2>Igor Musmen</h2>
-        <p class="title">Student/ Teamlead</p>
-        <p>Project structure,  environment setup, games "SpeakIt", "English-puzzle", "Main game (Linguist)", spaced repetitions logic, vocabulary and statistics pages.</p>
-        <p><button class="team__button">Github</button></p>
+        <p class="xteam__title">Student / Teamlead <a class="xteam__button" href="https://github.com/Musmen">Github</a></p>
+        <p>Project structure,  environment setup, games 'SpeakIt', 'English-puzzle', 'Main game (Linguist)', spaced repetitions logic, vocabulary and statistics pages, common loader, 
+        general methods for long-term statistics.</p>
       </div>
     </div>
   </div>
 
-  <div class="column">
-    <div class="card">
+  <div class="xteam__column">
+    <div class="xteam__card">
       <img src="https://avatars3.githubusercontent.com/u/40052037?s=460&u=a1197b12e0654838e9166fb8757c5da01f3d0cf1&v=4" alt="John" style="width:100%">
-      <div class="container">
+      <div class="xteam__container">
         <h2>Vanya Radivilov</h2>
-        <p class="title">Student</p>
-        <p>Design, markup (main app pages), main menu, routing, game "Sprint", own game ("Drop").</p>
-        <p><button class="team__button">Github</button></p>
+        <p class="xteam__title">Student <a href="https://github.com/nixon2105"><button class="xteam__button">Github</button></a></p>
+        <p>Design, markup (main app pages), main menu, routing, game 'Sprint', own game ('Drop').</p>
       </div>
     </div>
   </div>
 
-  <div class="column">
-    <div class="card">
+  <div class="xteam__column">
+    <div class="xteam__card">
       <img src="https://avatars2.githubusercontent.com/u/51196040?s=460&u=725fafe209c2dc69b906bb8ca180bd6b5054f2a8&v=4" alt="John" style="width:100%">
-      <div class="container">
+      <div class="xteam__container">
         <h2>Asiliia Tikhonycheva</h2>
-        <p class="title">Student</p>
-        <p>Services for API endpoints, login/logout toolbars and functionality, 
-        integration tests, error popup, game "Savanna", team page.</p>
-        <p><button class="team__button">Github</button></p>
+        <p class="xteam__title">Student <a href="https://github.com/Asiliia"><button class="xteam__button">Github</button></a></p>
+        <p>Services for API endpoints and Jest tests, game 'Savanna', login/logout UI and functionality, token validation, common error popup, team page.</p>
       </div>
     </div>
   </div>
 
-<div class="column">
-  <div class="card">
+<div class="xteam__column">
+  <div class="xteam__card">
     <img src="https://avatars3.githubusercontent.com/u/39918932?s=460&u=3a3344f94e9422e6760279d1592a7677b178314b&v=4" alt="John" style="width:100%">
-    <div class="container">
+    <div class="xteam__container">
       <h2>Max Shelepen</h2>
-      <p class="title">Student</p>
-      <p>Promo page, design, markup, game "Sprint".</p>
-      <p><button class="team__button">Github</button></p>
+      <p class="xteam__title">Student <a href="https://github.com/MaxShelepen"><button class="xteam__button">Github</button></a></p>
+      <p>Promo page, design, markup, game 'Sprint'.</p>
     </div>
   </div>
 </div>
 
-<div class="column">
-  <div class="card">
+<div class="xteam__column">
+  <div class="xteam__card">
     <img src="https://avatars0.githubusercontent.com/u/56892495?s=460&u=4c572bd97137c3c1ff8d7634b8ff6c3144ec6acf&v=4" alt="John" style="width:100%">
-    <div class="container">
+    <div class="xteam__container">
       <h2>Hleb Pihuleuski</h2>
-      <p class="title">Student</p>
+      <p class="xteam__title">Student <a href="https://github.com/CasperCarver"><button class="xteam__button">Github</button></a></p>
       <p>Environment setup, settings page, main intro page.</p>
-      <p><button class="team__button">Github</button></p>
     </div>
   </div>
 </div>
 
-<div class="column">
-  <div class="card">
+<div class="xteam__column">
+  <div class="xteam__card">
     <img src="https://avatars1.githubusercontent.com/u/59864226?s=460&u=dd800b1118a99d54beb08aad445d1ffd71781839&v=4" alt="John" style="width:100%">
-    <div class="container">
+    <div class="xteam__container">
       <h2>Misha Shiryakov</h2>
-      <p class="title">Student</p>
+      <p class="xteam__title">Student <a href="https://github.com/mishashiryakov"><button class="xteam__button">Github</button></a></p>
       <p>Game 'Audiocall'.</p>
-      <p><button class="team__button">Github</button></p>
     </div>
   </div>
 </div>
